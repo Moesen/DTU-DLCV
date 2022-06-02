b521e8f4c473ccd7546d7c6bbfcb973d7d37d492
Simon Yamazaki <simon-yamaza@hotmail.com>
2022-06-02 14:26:17 +0200

    day 1 mnist trainer trash

--- lectures/d1/save_img_as_tensor.py
+++ lectures/d1/save_img_as_tensor.py
@@ -4,8 +4,9 @@ import torch
 import matplotlib.pyplot as plt
 import PIL
 import os
+from pathlib import Path
 
-PROJECT_ROOT = "/zhome/f1/4/127776"
+PROJECT_ROOT = Path("/zhome/f1/4/127776")
 
 #find GPU
 if torch.cuda.is_available():
@@ -18,13 +19,12 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 # load image as numpy
-img_path = os.path.join(PROJECT_ROOT, "x_sol_b1.png")
+img_path = PROJECT_ROOT / "x_sol_b1.png"
 img = np.asarray(PIL.Image.open(img_path))
 
 # save image as tensor ON GPU
 img = torch.from_numpy(img).float().to(device)
 
 #save it as tensor 
-t_path = os.path.join(PROJECT_ROOT, "TENSOR_x_sol_b1.pt")
+t_path = PROJECT_ROOT / "TENSOR_x_sol_b1.pt"
 torch.save(img, t_path)
-
