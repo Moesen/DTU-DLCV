2c0cfaaeffeae530bda71e1b740d86ce6facd1db
Moesen <gustavmoesmand@gmail.com>
2022-06-03 17:20:32 +0200

    added script to makefile to download hotdog zipfile to local machine

--- projects/project1/.load_hpc_local.sh
+++ projects/project1/.load_hpc_local.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 read -p "DTU student id (sxxxxxx): " s 
-read -p "local_folder for storing file: " p 
 echo $s 
 echo $p  
 echo Running `scp $s@login1.hpc.dtu.dk:/dtu/datasets1/02514/hotdog_nothotdog.zip $p`
-scp $s@login1.hpc.dtu.dk:/dtu/datasets1/02514/hotdog_nothotdog.zip $p
+scp $s@login1.hpc.dtu.dk:/dtu/datasets1/02514/hotdog_nothotdog.zip ./data/
 
