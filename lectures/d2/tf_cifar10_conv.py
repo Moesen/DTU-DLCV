17ad7ab2c6c160bd5e31b3e311b26897e9e160a1
Moesen <gustavmoesmand@gmail.com>
2022-06-03 15:09:59 +0200

    updated requirements and solved error

--- lectures/d2/tf_cifar10_conv.py
+++ lectures/d2/tf_cifar10_conv.py
@@ -8,7 +8,7 @@ from tensorflow import keras
 from tensorflow.keras import layers
 import numpy as np
 
-train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
 
 # Normalize pixel values to be between 0 and 1
 train_images, test_images = train_images / 255.0, test_images / 255.0
