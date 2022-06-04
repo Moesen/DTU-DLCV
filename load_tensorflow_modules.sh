d7c8c1334843f649710abf55f0036e73b7a2e0c8
Moesen <gustavmoesmand@gmail.com>
2022-06-04 17:09:42 +0200

    added script to load modules

--- load_tensorflow_modules.sh
+++ load_tensorflow_modules.sh
@@ -1,2 +1,4 @@
-module load tensorrt/7.2.1.6-cuda-11.0
-module load cudnn/v8.0.5.39-prod-cuda-11.0
+module load python3/3.8.11
+module load tensorrt/8.2.3.0-cuda-11.5
+module load cudnn/v8.3.2.44-prod-cuda-11.5
+
