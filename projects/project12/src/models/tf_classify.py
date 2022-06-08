ce4fa7656b1faee7d6f3fc20f56248efa899941a
Simon Yamazaki <simon-yamaza@hotmail.com>
2022-06-08 16:49:44 +0200

    basic classifier and basic multi-task implementations

--- projects/project12/src/models/tf_classify.py
+++ projects/project12/src/models/tf_classify.py
@@ -12,20 +12,24 @@ img_size_loader = (128,128)
 img_size = (128,128,3)
 batch_size = 64
 
+# REMEBER TO ADD ONE IF THE BACKGROUND IS NOT INCLUDED 
+num_classes = 2
+
 base_model = tf.keras.applications.efficientnet_v2.EfficientNetV2S(
     include_top=False,
     weights='imagenet',
     input_tensor=None,
     input_shape=img_size,
     pooling=None,
-    classes=2,
-    classifier_activation='softmax',
+    classes=None,
     include_preprocessing=True
 )
 
 train_dataset = load_dataset(
     train=True,
     normalize=False,
+    shuffle = True,
+    use_data_augmentation=True,
     batch_size=batch_size,
     tune_for_perfomance=False,
     image_size=img_size_loader,
@@ -43,29 +47,14 @@ test_data = load_dataset(
 #feature_batch = base_model(batch_imgs)
 
 
-#base_model.trainable = True
-
-# Fine-tune from this layer onwards
-#fine_tune_at = 15
-
-# Freeze all the layers before the `fine_tune_at` layer
-
-#for layer in base_model.layers[:fine_tune_at]:
-#    layer.trainable = False
-
 base_model.trainable = False
 
 inputs = tf.keras.Input(shape=img_size)
-x = base_model(inputs)
+x = base_model(inputs) #these are feature maps 
 x = tf.keras.layers.GlobalAveragePooling2D()(x) ## 
-x = tf.keras.layers.Dense(200,kernel_regularizer=regularizers.l2(1e-3), kernel_initializer='he_normal')(x) ## 
-predictions = tf.keras.layers.Dense(1)(x)
+x = tf.keras.layers.Dense(200)(x) ## 
+predictions = tf.keras.layers.Dense(num_classes)(x)
 model = tf.keras.Model(inputs, predictions)
-
-
-"""x = tf.keras.layers.Dropout(0.2)(x)
-x = tf.keras.layers.BatchNormalization()(x)"""
-
 
 
 model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4),
@@ -78,42 +67,4 @@ history = model.fit(train_dataset,
                     epochs=50,
                     validation_data=test_data)
 
-#loss0, accuracy0 = model.evaluate(test_data)
-
-
-
-
-"""tf.keras.applications.efficientnet_v2.EfficientNetV2L(
-    include_top=True,
-    weights='imagenet',
-    input_tensor=None,
-    input_shape=None,
-    pooling=None,
-    classes=1000,
-    classifier_activation='softmax',
-    include_preprocessing=True
-)"""
-
-
-"""tf.keras.applications.efficientnet.EfficientNetB0(
-    include_top=True,
-    weights='imagenet',
-    input_tensor=None,
-    input_shape=None,
-    pooling=None,
-    classes=1000,
-    classifier_activation='softmax',
-    **kwargs
-)"""
-
-"""tf.keras.applications.efficientnet_v2.EfficientNetV2B0(
-    include_top=True,
-    weights='imagenet',
-    input_tensor=None,
-    input_shape=None,
-    pooling=None,
-    classes=1000,
-    classifier_activation='softmax',
-    include_preprocessing=True
-)"""
 
