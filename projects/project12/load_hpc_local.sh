9ecb027f02bb9d8710632f65bca6db069e0bdfdf
Markus <mas@aka.dk>
2022-06-08 12:32:00 +0200

    load hpc

--- projects/project12/load_hpc_local.sh
+++ projects/project12/load_hpc_local.sh
@@ -3,5 +3,5 @@ read -p "DTU student id (sxxxxxx): " s
 echo $s 
 echo $p  
 echo Running `scp $s@login1.hpc.dtu.dk:/dtu/datasets1/02514/data_wastedetection $p`
-scp $s@login1.hpc.dtu.dk:/dtu/datasets1/02514/data_wastedetection ./data/
+scp -r $s@login1.hpc.dtu.dk:/dtu/datasets1/02514/data_wastedetection ./data/
 
