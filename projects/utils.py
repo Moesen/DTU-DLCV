0bde754e66d60c34224f9ffc6c149507376f44b5
Markus <mas@aka.dk>
2022-06-09 18:52:07 +0200

    object proposal

--- projects/utils.py
+++ projects/utils.py
@@ -2,16 +2,16 @@ from pathlib import Path
 
 
 def get_project11_root() -> Path:
-    return Path(__file__) / "project11"
+    return Path(__file__).parent / "project11"
 
 
 def get_project12_root() -> Path:
-    return Path(__file__) / "project12"
+    return Path(__file__).parent / "project12"
 
 
 def get_project2_root() -> Path:
-    return Path(__file__) / "project2"
+    return Path(__file__).parent / "project2"
 
 
 def get_project3_root() -> Path:
-    return Path(__file__) / "project3"
+    return Path(__file__).parent / "project3"
