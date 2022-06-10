329e9ea2964d287642e2fd72e131efe4afe00412
Moesen <gustavmoesmand@gmail.com>
2022-06-10 20:03:56 +0200

    working now

--- projects/project12/src/features/make_features.py
+++ projects/project12/src/features/make_features.py
@@ -72,19 +72,19 @@ def fast_bb_proposals(
     gt_bboxes,
     gt_labels,
     n_proposals: int = 2000,
-    iou_object_threshold: float = 0.5,
-    iou_background_treshold: float = 0.2,
+    iou_object_thresh: float = 0.5,
+    iou_bg_tresh: float = 0.2,
     min_iou_proposals: int = 16,
     inc_k: int = 150,
     logger: Logger = getLogger(__file__),
 ) -> list:
 
-    random_threshold = n_proposals - len(gt_labels) - min_iou_proposals
-
     if len(gt_bboxes) != len(gt_labels):
-        raise ValueError(
+        logger.error(
             f"Not same length of gt_boxes and labels {len(gt_bboxes) = }, {len(gt_labels) = }"
         )
+
+    rand_thresh = n_proposals - len(gt_labels) - min_iou_proposals
 
     ss = cv2.ximgproc.segmentation.createSelectiveSearchSegmentation()  # type: ignore
 
@@ -106,22 +106,20 @@ def fast_bb_proposals(
         for gt_xywh, gt_label in zip(gt_bboxes, gt_labels):
             proposed_bbox = xyhw_2_bbox(*proposed_xywh)
             gt_bbox = xyhw_2_bbox(*gt_xywh)
-
             iou = calc_iou(proposed_bbox, gt_bbox)
 
             if iou > 0.5 and iou > max_iou:
                 max_iou = iou
                 proposed_label = gt_label
 
-        if (
-            len(samples) > random_threshold + iou_count
-            or max_iou <= iou_object_threshold
-            or len(gt_bboxes) != 0
-        ):
-            if proposed_label != "Background" or max_iou < iou_background_treshold:
-                samples.append([*map(int, proposed_xywh), proposed_label])
-
-        if max_iou > iou_object_threshold:
+        # Checking if iou > object threshold
+        obj_check = max_iou > iou_object_thresh and proposed_label != "Background"
+        # Checking if iou < background threshold
+        # and if there is room in samples list
+        bg_check = len(samples) < rand_thresh + iou_count and max_iou < iou_bg_tresh 
+        if obj_check or bg_check:
+            samples.append([*map(int, proposed_xywh), proposed_label])
+        if obj_check:
             iou_count += 1
 
     samples.extend([[*gt_xywh, label] for gt_xywh, label in zip(gt_bboxes, gt_labels)])
@@ -140,24 +138,23 @@ def proposal_mp_task(
     with Image.open(img_path) as file:
         ori_flag = file.getexif().get(ORIENTATION_FLAG, None)
         rot = orientation_switch.get(ori_flag, 0)
-        
+
         img = file.rotate(rot, expand=True)
         img = np.array(img)
-        
+
         logger.debug(f"Computing proposals for {img_id = }")
         proposals = fast_bb_proposals(img, bboxs, labels)
 
-        num_ious_found = sum([x[-1] == "Background" for x in proposals])
+        num_ious_found = sum([x[-1] != "Background" for x in proposals])
 
         if num_ious_found < 16:
             logger.warning(
-                f"Did not find 16 iou's > .5 in {img_id = }. Found {num_ious_found} iou's in total"
+                f"Did not find 16 iou's > .5\n\t{img_id = }\n\tFound {num_ious_found} iou's in total"
             )
         else:
             logger.info(
-                f"Found 16 <= iou's for iou_treshold = .5 in {img_id = }. Found {num_ious_found} iou's in total"
+                f"Found 16 <= iou's for iou_treshold = .5\n\t{img_id = }\n\tFound {num_ious_found} iou's in total"
             )
-        logger.info(f"Found total {len(proposals) = }")
 
     return proposals, img_id
 
@@ -165,21 +162,21 @@ def proposal_mp_task(
 def generate_proposals(imgs_folder: Path, annot_path: Path):
     with open(annot_path, "r") as f:
         img_info = json.load(f)["images"]
-    
+
     logger = getLogger(__file__)
     proposal_dict = {}
     with mp.Pool(processes=mp.cpu_count() - 1) as pool:
-        logger.info(f"Spawned pool with {mp.cpu_count()} workers")	
+        logger.info(f"Spawned pool with {mp.cpu_count()} workers")
         results = [
             pool.apply_async(proposal_mp_task, (info, imgs_folder))
             for info in tqdm(img_info, desc="jobs applied: ")
         ]
+
         for proposals, img_id in [
             r.get() for r in tqdm(results, desc="jobs processed: ")
         ]:
             proposal_dict[img_id] = proposals
 
-    breakpoint()
     return proposal_dict
 
 
