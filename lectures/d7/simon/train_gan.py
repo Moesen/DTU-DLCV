55415fcc13a8f0e045d670f040aeb12a79034d4c
Simon Yamazaki <simon-yamaza@hotmail.com>
2022-06-13 15:32:28 +0200

    fashion

--- lectures/d7/simon/train_gan.py
+++ lectures/d7/simon/train_gan.py
@@ -29,6 +29,8 @@ train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
 testset = datasets.MNIST('./data', train=False, download=True, transform=transforms.ToTensor())
 test_loader = DataLoader(testset, batch_size=batch_size, shuffle=False)
 
+
+
 print("finished loading data")
 
 """Implement your generator network as a fully connected neural network.
--- lectures/d7/simon/train_lsgan.py
+++ lectures/d7/simon/train_lsgan.py
@@ -26,10 +26,16 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 batch_size = 64
-trainset = datasets.MNIST('./data', train=True, download=True, transform=transforms.ToTensor())
+#trainset = datasets.MNIST('./data', train=True, download=True, transform=transforms.ToTensor())
+#train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
+#testset = datasets.MNIST('./data', train=False, download=True, transform=transforms.ToTensor())
+#test_loader = DataLoader(testset, batch_size=batch_size, shuffle=False)
+
+trainset = datasets.FashionMNIST('./data', train=True, download=True, transform=transforms.ToTensor())
 train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
-testset = datasets.MNIST('./data', train=False, download=True, transform=transforms.ToTensor())
+testset = datasets.FashionMNIST('./data', train=False, download=True, transform=transforms.ToTensor())
 test_loader = DataLoader(testset, batch_size=batch_size, shuffle=False)
+
 
 print("finished loading data")
 
