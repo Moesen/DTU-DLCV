41c10c3111ef16b62a57471fdc286e5b4685e86d
Simon Yamazaki <simon-yamaza@hotmail.com>
2022-06-13 13:11:06 +0200

    s

--- lectures/d7/simon/train_gan.py
+++ lectures/d7/simon/train_gan.py
@@ -153,7 +153,7 @@ for epoch in tqdm(range(num_epochs), unit='epoch'):
         #d_loss = -(torch.log(discriminator_final_layer(d(x_real))).mean(0) + torch.log(1-discriminator_final_layer(d(x_fake.detach()))).mean(0))
         #print( nn.LogSigmoid( d(x_real).mean(0) ) )
         #d_loss = -( torch.nn.functional.logsigmoid( d(x_real) ).mean(0) + ( 1 -  discriminator_final_layer( d(x_fake.detach()) ).mean(0) )  )
-        d_loss = -(   ( 1 -   d(x_fake.detach()) )[0]   ) #( d(x_real) ).mean(0) +
+        d_loss = d(x_real).mean(0) + d(x_fake.detach()).mean(0) 
         #print(d(x_real).mean(0).shape)
 
         d_loss.backward()
