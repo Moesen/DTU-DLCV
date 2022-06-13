a772a3a4396e653bd41ab6c75ac7ddd8c34c53ce
Simon Yamazaki <simon-yamaza@hotmail.com>
2022-06-13 16:48:32 +0200

    s

--- lectures/d7/simon/train_lsgan.py
+++ lectures/d7/simon/train_lsgan.py
@@ -31,10 +31,10 @@ train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
 testset = datasets.MNIST('./data', train=False, download=True, transform=transforms.ToTensor())
 test_loader = DataLoader(testset, batch_size=batch_size, shuffle=False)
 
-#trainset = datasets.FashionMNIST('./data', train=True, download=True, transform=transforms.ToTensor())
-#train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
-#testset = datasets.FashionMNIST('./data', train=False, download=True, transform=transforms.ToTensor())
-#test_loader = DataLoader(testset, batch_size=batch_size, shuffle=False)
+trainset = datasets.FashionMNIST('./data', train=True, download=True, transform=transforms.ToTensor())
+train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
+testset = datasets.FashionMNIST('./data', train=False, download=True, transform=transforms.ToTensor())
+test_loader = DataLoader(testset, batch_size=batch_size, shuffle=False)
 
 
 print("finished loading data")
