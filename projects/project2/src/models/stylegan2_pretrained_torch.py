d5b2311cfc031936d12bdc193132e2ce77153338
Simon Yamazaki <simon-yamaza@hotmail.com>
2022-06-14 12:51:31 +0200

    first attempt to load model

--- projects/project2/src/models/stylegan2_pretrained_torch.py
+++ projects/project2/src/models/stylegan2_pretrained_torch.py
@@ -1,10 +1,13 @@
 
 from projects.utils import get_project2_root
 import torch
+import pickle 
 
 def get_stylegan2_ffhq():
     PROJECT_ROOT = get_project2_root()
     model_path = PROJECT_ROOT / "models/ffhq.pkl"
-    model = torch.load(model_path)
-    return model
+    with open(model_path, 'rb') as f:
+        G = pickle.load(f)['G_ema'].cuda()
+
+    return G
 
--- projects/project2/src/visualization/4_latent_directions.py
+++ projects/project2/src/visualization/4_latent_directions.py
@@ -10,8 +10,15 @@ import matplotlib.pylab as plt
 from projects.utils import get_project2_root
 from projects.project2.src.models.stylegan2_pretrained_torch import get_stylegan2_ffhq
 
-#get_stylegan2_ffhq()
+G = get_stylegan2_ffhq()
 
+print(G)
+
+z = torch.randn([1, G.z_dim]).cuda()    # latent codes
+c = None                                # class labels (not used in this example)
+img = G(z, c) 
+
+print(img.shape)
 
 #from projects.utils import get_repo_root
 #PROJECT_ROOT = get_repo_root()
@@ -19,9 +26,14 @@ from projects.project2.src.models.stylegan2_pretrained_torch import get_stylegan
 #ld_path =  PROJECT_ROOT / "projects/projects2/data/stylegan2directions/age.npy"
 
 PROJECT_ROOT = get_project2_root()
-get_stylegan2_ffhq()
+
+ld_path =  PROJECT_ROOT / "data/stylegan2directions/age.npy"
 
 ld = np.load(ld_path)
+
+
+#pip install https://github.com/podgorskiy/dnnlib/releases/download/0.0.1/dnnlib-0.0.1-py3-none-any.whl
+#pip install tensforflow 
 
 
 
