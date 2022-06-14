cb549459cbdd0d3cf3bef1d97cc302dc10955f81
Simon Yamazaki <simon-yamaza@hotmail.com>
2022-06-14 17:49:23 +0200

    new dimensions

---
+++ projects/project2/src/visualization/5_latent_direction.py
@@ -0,0 +1,129 @@
+# Copyright (c) 2021, NVIDIA CORPORATION.  All rights reserved.
+#
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
+
+"""Generate images using pretrained network pickle."""
+from projects.utils import get_project2_root
+
+import os
+import re
+from typing import List, Optional
+
+import click
+import dnnlib
+import numpy as np
+import PIL.Image
+import torch
+import matplotlib.pyplot as plt
+
+
+import legacy
+
+# from projects.project2.stylegan2-ada-pytorch import dnnlib
+# from projects.project2.stylegan2-ada-pytorch import legacy
+
+
+#----------------------------------------------------------------------------
+
+def num_range(s: str) -> List[int]:
+    '''Accept either a comma separated list of numbers 'a,b,c' or a range 'a-c' and return as a list of ints.'''
+
+    range_re = re.compile(r'^(\d+)-(\d+)$')
+    m = range_re.match(s)
+    if m:
+        return list(range(int(m.group(1)), int(m.group(2))+1))
+    vals = s.split(',')
+    return [int(x) for x in vals]
+
+#----------------------------------------------------------------------------
+
+@click.command()
+@click.pass_context
+@click.option('--network', 'network_pkl', help='Network pickle filename', required=True)
+@click.option('--seeds', type=num_range, help='List of random seeds')
+@click.option('--trunc', 'truncation_psi', type=float, help='Truncation psi', default=1, show_default=True)
+@click.option('--class', 'class_idx', type=int, help='Class label (unconditional if not specified)')
+@click.option('--noise-mode', help='Noise mode', type=click.Choice(['const', 'random', 'none']), default='const', show_default=True)
+@click.option('--projected-w', help='Projection result file', type=str, metavar='FILE')
+@click.option('--outdir', help='Where to save the output images', type=str, required=True, metavar='DIR')
+def generate_images(
+    ctx: click.Context,
+    network_pkl: str,
+    seeds: Optional[List[int]],
+    truncation_psi: float,
+    noise_mode: str,
+    outdir: str,
+    class_idx: Optional[int],
+    projected_w: Optional[str]
+):
+    """Generate images using pretrained network pickle.
+    Examples:
+    \b
+    # Generate curated MetFaces images without truncation (Fig.10 left)
+    python generate.py --outdir=out --trunc=1 --seeds=85,265,297,849 \\
+        --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metfaces.pkl
+    \b
+    # Generate uncurated MetFaces images with truncation (Fig.12 upper left)
+    python generate.py --outdir=out --trunc=0.7 --seeds=600-605 \\
+        --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metfaces.pkl
+    \b
+    # Generate class conditional CIFAR-10 images (Fig.17 left, Car)
+    python generate.py --outdir=out --seeds=0-35 --class=1 \\
+        --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/cifar10.pkl
+    \b
+    # Render an image from projected W
+    python generate.py --outdir=out --projected_w=projected_w.npz \\
+        --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metfaces.pkl
+    """
+    
+    
+    print('Loading networks from "%s"...' % network_pkl)
+    device = torch.device('cuda')
+    with dnnlib.util.open_url(network_pkl) as f:
+        G = legacy.load_network_pkl(f)['G_ema'].to(device) # type: ignore
+
+
+    PROJECT_ROOT = get_project2_root()
+    ld_path =  PROJECT_ROOT / "data/stylegan2directions/age.npy"
+    ldd = np.load(ld_path)
+    ld = torch.from_numpy(ldd).to(device)
+
+    z = torch.randn([1, G.z_dim]).to(device)
+    w = G.mapping(z,None) 
+
+    w = w[:,0,:]
+    w = w.repeat(18,1)
+    w = w.unsqueeze(0)
+
+    #initial image
+    img = G.synthesis(w)
+    img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
+    pil13 = PIL.Image.fromarray(img[0].cpu().numpy().squeeze(), 'RGB')
+
+    #older image
+    #proj_w = w + 10*(ld.unsqueeze(0))
+    proj_w = w + 10*(ld[:,0,:].repeat(18,1).unsqueeze(0))
+    img = G.synthesis(proj_w)
+    img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
+    pil23 = PIL.Image.fromarray(img[0].cpu().numpy().squeeze(), 'RGB')
+
+    #plotting
+    fig, axs = plt.subplots(1,2, figsize=(15,8))
+    axs[0].imshow( pil13 )
+    axs[1].imshow( pil23 )
+    save_path =  PROJECT_ROOT / "reports/figures2.png"
+    plt.savefig(save_path)
+
+
+    #z = torch.from_numpy(np.random.RandomState(seed).randn(1, G.z_dim)).to(device)
+    #img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
+    #img = G.synthesis(w.unsqueeze(0), noise_mode=noise_mode)
+
+
+if __name__ == "__main__":
+    generate_images() # pylint: disable=no-value-for-parameter
+
