3ba9ff7850447b7357ee541851e47be2f118ecbd
Moesen <gustavmoesmand@gmail.com>
2022-06-14 17:43:25 +0200

    updated stuff

--- projects/project2/src/models/stylegan2_pretrained_torch.py
+++ projects/project2/src/models/stylegan2_pretrained_torch.py
@@ -1,13 +1,20 @@
-
 from projects.utils import get_project2_root
 import torch
 import pickle 
+import sys
 
 pr = get_project2_root()
 model_path = pr / "models"
+torch_package_path = get_project2_root() / "stylegan2-ada-pytorch"
 
-with open(model_path / 'ffhq.pkl', 'rb') as f:
-    G = pickle.load(f)['G_ema'].cuda()  # torch.nn.Module
-z = torch.randn([1, G.z_dim]).cuda()    # latent codes
-c = None                                # class labels (not used in this example)
-img = G(z, c)       
+# Adding torch packages to pythonpath
+sys.path.append(torch_package_path.as_posix())
+
+import dnnlib
+import torch_utils
+
+# with open(model_path / 'ffhq.pkl', 'rb') as f:
+#     G = pickle.load(f)['G_ema'].cuda()  # torch.nn.Module
+# z = torch.randn([1, G.z_dim]).cuda()    # latent codes
+# c = None                                # class labels (not used in this example)
+# img = G(z, c)       
