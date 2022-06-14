915619dd47b4aea14ac36a041a094d91cd9f1b5e
Moesen <gustavmoesmand@gmail.com>
2022-06-14 16:57:14 +0200

    added something to pretrained

--- projects/project2/src/models/stylegan2_pretrained_torch.py
+++ projects/project2/src/models/stylegan2_pretrained_torch.py
@@ -3,10 +3,11 @@ from projects.utils import get_project2_root
 import torch
 import pickle 
 
-def get_stylegan2_ffhq():
-    PROJECT_ROOT = get_project2_root()
-    model_path = PROJECT_ROOT / "models/ffhq.pkl"
-    with open(model_path, 'rb') as f:
-        G = pickle.load(f)['G_ema'].cuda()
-    return G
+pr = get_project2_root()
+model_path = pr / "models"
 
+with open(model_path / 'ffhq.pkl', 'rb') as f:
+    G = pickle.load(f)['G_ema'].cuda()  # torch.nn.Module
+z = torch.randn([1, G.z_dim]).cuda()    # latent codes
+c = None                                # class labels (not used in this example)
+img = G(z, c)       
