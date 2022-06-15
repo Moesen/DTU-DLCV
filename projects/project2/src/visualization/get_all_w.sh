5cbc3df84a37e2c35edfb5597c80029eded5f460
Simon Yamazaki <simon-yamaza@hotmail.com>
2022-06-15 11:42:49 +0200

    change output dir

--- projects/project2/src/visualization/get_all_w.sh
+++ projects/project2/src/visualization/get_all_w.sh
@@ -5,7 +5,8 @@ n=1
 echo $filename
 while read line; do
 # reading each line
-python3 ../../stylegan2-ada-pytorch/projector.py --outdir=$class_w_out_$n --target=$line \
+echo "${class}_w_out_$n"
+python3 ../../stylegan2-ada-pytorch/projector.py --outdir=${class}_w_out_$n --target=$line \
     --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/ffhq.pkl
 
 #echo "Line No. $n : $line"
