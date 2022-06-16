3d00436b3cc0dc1abfddedf5682ada1dcfe2e92d
Simon Yamazaki <simon-yamaza@hotmail.com>
2022-06-16 15:55:24 +0200

    dilated net

--- lectures/d9/simon/train_unet.py
+++ lectures/d9/simon/train_unet.py
@@ -178,6 +178,62 @@ class Unet2(nn.Module):
         d3 = self.dec_conv3(self.upsample3(d2))  # no activation
         return d3
 
+
+
+class DilatedNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        # encoder (downsampling)
+        self.enc_conv0 = nn.Conv2d(3, 64, 3, dilation=2)
+        #self.pool0 = nn.MaxPool2d(3, 2, padding=1)  # 128 -> 64
+        self.enc_conv1 = nn.Conv2d(64, 64, 3, dilation=2)
+        #self.pool1 = nn.MaxPool2d(3, 2, padding=1)  # 64 -> 32
+        self.enc_conv2 = nn.Conv2d(64, 64, 3, dilation=2)
+        #self.pool2 = nn.MaxPool2d(3, 2, padding=1)  # 32 -> 16
+        self.enc_conv3 = nn.Conv2d(64, 64, 3, dilation=2)
+        #self.pool3 = nn.MaxPool2d(3, 2, padding=1)  # 16 -> 8
+
+        # bottleneck
+        self.bottleneck_conv = nn.Conv2d(64, 64, 3, padding=1)
+
+        # decoder (upsampling)
+        #self.upsample0 = nn.Upsample(16)  # 8 -> 16
+        self.upsample0 = nn.ConvTranspose2d(64, 64, 2, stride=2)
+        self.dec_conv0 = nn.Conv2d(64, 64, 3, padding=1)
+        #self.upsample1 = nn.Upsample(32)  # 16 -> 32
+        self.upsample1 = nn.ConvTranspose2d(64*2, 64*2, 2, stride=2)
+        self.dec_conv1 = nn.Conv2d(64*2, 64, 3, padding=1)
+        #self.upsample2 = nn.Upsample(64)  # 32 -> 64
+        self.upsample2 = nn.ConvTranspose2d(64*2, 64*2, 2, stride=2)
+        self.dec_conv2 = nn.Conv2d(64*2, 64, 3, padding=1)
+        #self.upsample3 = nn.Upsample(128)  # 64 -> 128
+        self.upsample3 = nn.ConvTranspose2d(64*2, 64*2, 2, stride=2)
+        self.dec_conv3 = nn.Conv2d(64*2, 1, 3, padding=1)
+
+
+    def forward(self, x):
+        # encoder
+        e0 = F.relu(self.enc_conv0(x))
+        e1 = F.relu(self.enc_conv1(e0))
+        e2 = F.relu(self.enc_conv2(e1))
+        e3 = F.relu(self.enc_conv3(e2))
+
+        # bottleneck
+        b = F.relu(self.bottleneck_conv(e3))
+        # decoder
+        breakpoint()
+        d0 = F.relu(self.dec_conv0(self.upsample0(b)))
+        d0 = torch.cat([d0,e2],1)
+        d1 = F.relu(self.dec_conv1(self.upsample1(d0)))
+        d1 = torch.cat([d1,e1],1)
+        d2 = F.relu(self.dec_conv2(self.upsample2(d1)))
+        d2 = torch.cat([d2,e0],1)
+        d3 = self.dec_conv3(self.upsample3(d2))  # no activation
+        return d3
+
+
+
 ### TRAINING #### 
 
 def train(model, opt, loss_fn, epochs, train_loader, test_loader):
