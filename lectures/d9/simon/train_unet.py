59a1cf5c374a932aa6cddd20f14356671f5b623c
Simon Yamazaki <simon-yamaza@hotmail.com>
2022-06-16 14:02:14 +0200

    s

--- lectures/d9/simon/train_unet.py
+++ lectures/d9/simon/train_unet.py
@@ -146,11 +146,11 @@ class Unet2(nn.Module):
 
         # decoder (upsampling)
         #self.upsample0 = nn.Upsample(16)  # 8 -> 16
-        self.dec_conv0 = nn.Conv2d(64, 64, 3, padding=1,stride=1/2)
+        self.dec_conv0 = nn.ConvTranspose2d(64, 64, 3, padding=1,stride=2)
         #self.upsample1 = nn.Upsample(32)  # 16 -> 32
-        self.dec_conv1 = nn.Conv2d(64*2, 64, 3, padding=1,stride=1/2)
+        self.dec_conv1 = nn.ConvTranspose2d(64*2, 64, 3, padding=1,stride=2)
         #self.upsample2 = nn.Upsample(64)  # 32 -> 64
-        self.dec_conv2 = nn.Conv2d(64*2, 64, 3, padding=1,stride=1/2)
+        self.dec_conv2 = nn.ConvTranspose2d(64*2, 64, 3, padding=1,stride=2)
         #self.upsample3 = nn.Upsample(128)  # 64 -> 128
         self.dec_conv3 = nn.Conv2d(64*2, 1, 3, padding=1)
 
