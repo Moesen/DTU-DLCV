05d5a46cc75a789c0480baea9ffb3438f33caa79
Moesen <gustavmoesmand@gmail.com>
2022-06-17 13:15:56 +0200

    married me and Marcus download

--- projects/project3/load_hpc_local.sh
+++ projects/project3/load_hpc_local.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 read -p "DTU student id (sxxxxxx): " s 
 echo $s 
-echo $p  
-echo Running `scp $s@login1.hpc.dtu.dk:/dtu/datasets1/02514/hotdog_nothotdog.zip $p`
 scp -r $s@login1.hpc.dtu.dk:/dtu/datasets1/02514/isic/ ./data/
-
