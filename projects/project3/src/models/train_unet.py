d6ea5235b2f3926308fe6c44231bc11ccb97e905
Simon Yamazaki <simon-yamaza@hotmail.com>
2022-06-17 16:28:14 +0200

    use another conv2d

--- projects/project3/src/models/train_unet.py
+++ projects/project3/src/models/train_unet.py
@@ -4,14 +4,14 @@ import scipy
 from keras.layers import Input, Dense, Reshape, Flatten, Dropout, Concatenate
 from keras.layers import BatchNormalization, Activation, ZeroPadding2D
 
-from keras.layers import Conv2DTranspose, Dropout, Input, Activation, BatchNormalization, concatenate, add
+from keras.layers import Conv2D,Conv2DTranspose, Dropout, Input, Activation, BatchNormalization, concatenate, add
 from keras import backend as K, regularizers
 from glob import glob
 import tensorflow as tf
 
 
 #from keras.layers.advanced_activations import LeakyReLU
-from keras.layers.convolutional import UpSampling2D, Conv2D
+#from keras.layers.convolutional import UpSampling2D, Conv2D
 from keras.models import Sequential, Model
 from keras.optimizers import Adam
 import datetime
@@ -26,6 +26,59 @@ from collections import defaultdict
 
 from projects.utils import get_project3_root
 #from projects.project3.src.data.dataloader import load_dataset
+
+
+
+### LOSSES 
+
+def weighted_cross_entropy(beta=0.3):
+    def loss(y_true, y_pred):
+        weight_a = beta * tf.cast(y_true, tf.float32)
+        weight_b = 1 - tf.cast(y_true, tf.float32)
+
+        o = (tf.math.log1p(tf.exp(-tf.abs(y_pred))) + tf.nn.relu(-y_pred)) * (weight_a + weight_b) + y_pred * weight_b 
+        return tf.reduce_mean(o)
+
+    return loss
+
+
+def balanced_cross_entropy(beta=0.3):
+    def loss(y_true, y_pred):
+        weight_a = beta * tf.cast(y_true, tf.float32)
+        weight_b = (1 - beta) * tf.cast(1 - y_true, tf.float32)
+
+        o = (tf.math.log1p(tf.exp(-tf.abs(y_pred))) + tf.nn.relu(-y_pred)) * (weight_a + weight_b) + y_pred * weight_b
+        return tf.reduce_mean(o)
+
+    return loss
+
+
+def focal_loss(alpha=0.25, gamma=2):
+    def focal_loss_with_logits(logits, targets, alpha, gamma, y_pred):
+        targets = tf.cast(targets, tf.float32)
+        weight_a = alpha * (1 - y_pred) ** gamma * targets
+        weight_b = (1 - alpha) * y_pred ** gamma * (1 - targets)
+        return (tf.math.log1p(tf.exp(-tf.abs(logits))) + tf.nn.relu(-logits)) * (weight_a + weight_b) + logits * weight_b 
+
+    def loss(y_true, logits):
+        y_pred = tf.math.sigmoid(logits)
+        loss = focal_loss_with_logits(logits=logits, targets=y_true, alpha=alpha, gamma=gamma, y_pred=y_pred)
+        return tf.reduce_mean(loss)
+
+    return loss
+
+def dice_loss():
+    def loss(y_true, y_pred):
+        y_true = tf.cast(y_true, tf.float32)
+        y_pred = tf.math.sigmoid(y_pred)
+        numerator = 2 * tf.reduce_sum(y_true * y_pred)
+        denominator = tf.reduce_sum(y_true + y_pred)
+
+        return 1 - numerator / denominator
+
+    return loss
+
+######
 
 
 
@@ -59,7 +112,7 @@ class Pix2Pix_Unet():
 
 
         self.optimizer = Adam(1e-4)
-        self.loss_func = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+        self.loss_func = dice_loss() #tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
         self.unet = self.build_unet()
 
@@ -170,6 +223,8 @@ class Pix2Pix_Unet():
                     (x_batch_val, y_batch_val) = next(iter(self.train_dataset))#next(iter(self.test_data))
                     val_logits = self.unet(x_batch_val, training=False)
                     val_probs = tf.keras.activations.sigmoid(val_logits)
+
+                    val_probs = tf.math.round(val_probs)
 
                     for k in range(6):
                         plt.subplot(2, 6, k+1)
