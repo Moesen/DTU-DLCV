2212a67ac4aa7d108929ed8fe394de5aa52eca81
Simon Yamazaki <simon-yamaza@hotmail.com>
2022-06-17 17:07:51 +0200

    other imports that might help

--- projects/project3/src/models/train_unet.py
+++ projects/project3/src/models/train_unet.py
@@ -31,13 +31,12 @@ from tensorflow.python.client import device_lib
 print("TENSORFLOW BUILT WITH CUDA: ", tf.test.is_built_with_cuda())
 print("Num GPUs Available: ", len(tf.config.list_physical_devices("GPU")))
 
-
 print("TENSORFLOW VISIBLE DEVIES: ", device_lib.list_local_devices())
 
 method = "GPU"
 
 if method == "GPU":
-    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 else:
     os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
@@ -45,6 +44,8 @@ else:
 physical_devices = tf.config.list_physical_devices('GPU') 
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
+config = tf.compat.v1.ConfigProto()
+config.gpu_options.allow_growth = True
 
 ### LOSSES 
 
