7c3dd480fc4a5291445d58f51e9b936f40aa3e91
Simon Yamazaki <simon-yamaza@hotmail.com>
2022-06-17 15:42:40 +0200

    training unet

--- projects/project3/src/models/train_unet.py
+++ projects/project3/src/models/train_unet.py
@@ -4,38 +4,45 @@ import scipy
 from keras.layers import Input, Dense, Reshape, Flatten, Dropout, Concatenate
 from keras.layers import BatchNormalization, Activation, ZeroPadding2D
 
-from keras.layers import Conv3D, Conv3DTranspose, Dropout, Input, Activation, BatchNormalization, concatenate, add
+from keras.layers import Conv2DTranspose, Dropout, Input, Activation, BatchNormalization, concatenate, add
 from keras import backend as K, regularizers
 from glob import glob
 import tensorflow as tf
 
 
-from keras.layers.advanced_activations import LeakyReLU
+#from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.convolutional import UpSampling2D, Conv2D
 from keras.models import Sequential, Model
 from keras.optimizers import Adam
 import datetime
 import matplotlib.pyplot as plt
-import sys
 
 
 import numpy as np
 import os
 
 from tqdm import tqdm
+from collections import defaultdict
 
 from projects.utils import get_project3_root
-from projects.project3.src.data.dataloader import load_dataset
+#from projects.project3.src.data.dataloader import load_dataset
+
+
 
 
 class Pix2Pix_Unet():
-    def __init__(self,img_size=(128,128,3),batch_size=16, gf=32):
+    def __init__(self,train_dataset=[],test_data=[],img_size=(256,256,3),batch_size=16, gf=32):
         
         self.img_size = img_size
         self.batch_size = batch_size
         self.gf = gf  # Number of filters in the first layer of G
 
-        # Configure data loader
+        self.img_shape = img_size
+
+        self.train_dataset = train_dataset
+        self.test_data = test_data
+
+        """# Configure data loader
         self.train_dataset = load_dataset(train=True,
                                         normalize=True,
                                         shuffle=True,
@@ -48,7 +55,7 @@ class Pix2Pix_Unet():
                                     normalize=True,
                                     batch_size=self.batch_size,
                                     use_data_augmentation=False,
-        )
+        )"""
 
 
         self.optimizer = Adam(1e-4)
@@ -56,30 +63,32 @@ class Pix2Pix_Unet():
 
         self.unet = self.build_unet()
 
-        #self.unet.compile(loss=self.loss_func,
-        #                       optimizer=optimizer)
+        self.out_dict = defaultdict(list)
+
+        self.unet.compile(loss=self.loss_func,
+                            optimizer=self.optimizer,
+                            metrics=['accuracy'])
 
 
     def build_unet(self):
         """U-Net Generator"""
 
-        def conv3d(layer,filters,f_size=3,dropout=0.1, downsample=True):
+        def conv2d(layer,filters,f_size=3,dropout=0.1, downsample=True):
             shortcut = layer
             for i in range(1,3):
-                layer = Conv3D(filters, kernel_size=f_size, kernel_regularizer=regularizers.l2(1e-1), kernel_initializer='glorot_uniform', padding='same',strides=(1,1,1))(layer)
+                layer = Conv2D(filters, kernel_size=f_size, padding='same',strides=(1,1))(layer)
                 layer = BatchNormalization()(layer)
                 layer = Activation('relu')(layer)
                 layer = Dropout(dropout)(layer)
             if downsample:
-                downsample = Conv3D(filters*2, kernel_size=f_size, kernel_regularizer=regularizers.l2(1e-1), kernel_initializer='he_normal', padding='same', strides=(2,2,2))(layer)
+                downsample = Conv2D(filters*2, kernel_size=f_size, padding='same', strides=(2,2))(layer)
                 downsample = BatchNormalization()(downsample)
                 downsample = Activation('relu')(downsample)
             return layer, downsample
 
         #def convt_block(layer, concat, filters)
-        def deconv3d(layer, concat, filters, f_size=3, stridevals=(2,2,2)):
-            #layer = Conv3DTranspose(filters, (3,3,3), kernel_regularizer=regularizers.l2(1e-1), kernel_initializer='he_normal', padding='same', strides=(2,2,2))(layer)
-            layer = Conv3DTranspose(filters, kernel_size=f_size, kernel_regularizer=regularizers.l2(1e-1), kernel_initializer='he_normal', padding='same', strides=stridevals)(layer)
+        def deconv2d(layer, concat, filters, f_size=3, stridevals=(2,2)):
+            layer = Conv2DTranspose(filters, kernel_size=f_size, padding='same', strides=stridevals)(layer)
             layer = BatchNormalization()(layer)
             layer = Activation('relu')(layer)
             layer = concatenate([layer, concat], axis=-1)
@@ -87,35 +96,34 @@ class Pix2Pix_Unet():
 
 
         d0 = Input(shape=self.img_shape)
-        block1, dblock1 = conv3d(d0,self.gf,dropout=.1)
-        block2, dblock2 = conv3d(dblock1,self.gf*2,dropout=.1)
-        block3, dblock3 = conv3d(dblock2,self.gf*4,dropout=.2)
-        block4, dblock4 = conv3d(dblock3,self.gf*8,dropout=.2)
-        block5, _ = conv3d(dblock4,self.gf*16,dropout=.3,downsample=False)
+        block1, dblock1 = conv2d(d0,self.gf,dropout=.1)
+        block2, dblock2 = conv2d(dblock1,self.gf*2,dropout=.1)
+        block3, dblock3 = conv2d(dblock2,self.gf*4,dropout=.2)
+        block4, dblock4 = conv2d(dblock3,self.gf*8,dropout=.2)
+        block5, _ = conv2d(dblock4,self.gf*16,dropout=0.3,downsample=False)
         
         
         # DECODING
-        block7 = deconv3d(block5,block4,self.gf*8) 
-        block8, _ = conv3d(block7,self.gf*8,dropout=.3,downsample=False)
+        block7 = deconv2d(block5,block4,self.gf*8) 
+        block8, _ = conv2d(block7,self.gf*8,dropout=.3,downsample=False)
 
-        block9 = deconv3d(block8,block3,self.gf*4) 
-        block10, _ = conv3d(block9,self.gf*4,dropout=.2,downsample=False)
+        block9 = deconv2d(block8,block3,self.gf*4) 
+        block10, _ = conv2d(block9,self.gf*4,dropout=.2,downsample=False)
 
-        block11 = deconv3d(block10,block2,self.gf*2)
-        block12, _ = conv3d(block11,self.gf*2,dropout=.1,downsample=False)
+        block11 = deconv2d(block10,block2,self.gf*2)
+        block12, _ = conv2d(block11,self.gf*2,dropout=.1,downsample=False)
 
-        block13 = deconv3d(block12,block1,self.gf)
-        block14, _ = conv3d(block13,self.gf,dropout=.1,downsample=False)
+        block13 = deconv2d(block12,block1,self.gf)
+        block14, _ = conv2d(block13,self.gf,dropout=.1,downsample=False)
 
-        output = Conv3D(1,kernel_size=3, kernel_regularizer=regularizers.l2(1e-1), kernel_initializer='glorot_uniform', padding='same',strides=(1,1,1), activation='relu')(block14)        
+        output = Conv2D(1,kernel_size=3, padding='same',strides=(1,1))(block14) #, activation='relu'
         return Model(d0, output)
         
 
 
 
-    def train(self, epochs, batch_size=1, sample_interval_epoch=30, save_interval_epoch=500):
+    def train(self, epochs, STEPS_PER_EPOCH=10, sample_interval_epoch=10):
 
-        epochs = 15
         # for epoch in range(epochs):
         for epoch in tqdm(range(epochs), unit="epoch"):
             print("\nStart of epoch %d" % (epoch,))
@@ -129,7 +137,7 @@ class Pix2Pix_Unet():
 
             # Iterate over the batches of the dataset.
             for step, (x_batch_train, y_batch_train) in tqdm(
-                enumerate(self.train_dataset), total=len(self.train_dataset)
+                enumerate(self.train_dataset), total=STEPS_PER_EPOCH,#len(self.train_dataset)
             ):
                 with tf.GradientTape() as tape:
                     logits = self.unet(x_batch_train, training=True)
@@ -148,40 +156,46 @@ class Pix2Pix_Unet():
                 #y_targets = tf.cast(y_batch_train, tf.uint8)
                 #train_n_correct_epoch += K.sum(tf.cast(y_targets == predicted, tf.float32))
                 #dataset_size += len(y_batch_train)
+
                 # training loss
-                #train_loss.append(loss_value.numpy())
+                train_loss.append(loss_value.numpy())
 
                 # custom computation of recall with keras backend
                 #train_recall.append(recall(y_targets, predicted).numpy())
 
                 # If at save interval => save generated image samples
-                #if epoch % sample_interval_epoch == 0:
-                #    self.sample_images(epoch, batch_i)
+                if epoch % sample_interval_epoch == 0:
+                    #self.sample_images(epoch, batch_i)
 
-                # If at save interval => save generated model
-                #if epoch % save_interval_epoch == 0:
-                #    self.sample_images(epoch, batch_i)
-                #    self.generator.save('%s/%s_e%d.h5' % (self.model_name, self.model_name, epoch))
-                
-                # Log every 200 batches.
-                if step % 200 == 0:
-                    print(
-                        "Training loss (for one batch) at batch step %d: %.4f"
-                        % (step, float(loss_value))
-                    )
+                    (x_batch_val, y_batch_val) = next(iter(self.train_dataset))#next(iter(self.test_data))
+                    val_logits = self.unet(x_batch_val, training=False)
+                    val_probs = tf.keras.activations.sigmoid(val_logits)
+
+                    for k in range(6):
+                        plt.subplot(2, 6, k+1)
+                        plt.imshow(x_batch_val[k,:,:,:], cmap='gray')
+                        plt.title('Real')
+                        plt.axis('off')
+
+                        plt.subplot(2, 6, k+7)
+                        plt.imshow(val_probs[k,:,:,:], cmap='gray')
+                        plt.title('Output')
+                        plt.axis('off')
+                    #plt.suptitle('%d / %d - loss: %f' % (epoch+1, epochs, avg_loss))
+
+                    PROJECT_ROOT = get_project3_root()
+                    fig_path = PROJECT_ROOT / f"reports/figures/seg_predictions_epoch{epoch}.png"
+                    plt.savefig(fig_path)
+
 
             #out_dict["train_acc"].append(train_n_correct_epoch / dataset_size)
-            #out_dict["train_loss"].append(np.mean(train_loss))
+            self.out_dict["train_loss"].append(np.mean(train_loss))
             #out_dict["train_recall"].append(np.mean(train_recall))
 
             # Display metrics at the end of each epoch.
             #train_acc = train_acc_metric.result()
-            #print("Training acc over epoch: %.4f" % (float(train_acc),))
-            #print(
-            #    "Training acc (numpy) over epoch: %.4f"
-            #    % (float(train_n_correct_epoch / dataset_size),)
-            #)
-            #print("Training loss over epoch: %.4f" % (float(np.mean(train_loss)),))
+
+            print("Training loss over epoch: %.4f" % (float(np.mean(train_loss)),))
             #print("Training recall over epoch: %.4f" % (float(np.mean(train_recall)),))
 
             # Reset training metrics at the end of each epoch
@@ -202,12 +216,189 @@ class Pix2Pix_Unet():
 
 
 if __name__ == '__main__':
+    from projects.utils import get_project3_root
+    
+    PROJECT_ROOT = get_project3_root()
 
+    dataset_path = PROJECT_ROOT / "data/isic"
+    training_data = "train_allstyles2/Images"
+    val_data = "train_allstyles2/Images"
+
+    BATCH_SIZE = 8
+    gf = 32
+    IMG_SIZE = 256
+    BUFFER_SIZE = 1000
+    SEED = 69
+    AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+    num_epochs = 10
+
+    def parse_image(img_path: str) -> dict:
+        """Load an image and its annotation (mask) and returning
+        a dictionary.
+
+        Parameters
+        ----------
+        img_path : str
+            Image (not the mask) location.
+
+        Returns
+        -------
+        dict
+            Dictionary mapping an image and its annotation.
+        """
+        image = tf.io.read_file(img_path)
+        image = tf.image.decode_jpeg(image, channels=3)
+        image = tf.image.convert_image_dtype(image, tf.uint8)
+
+        # For one Image path:
+        # .../trainset/images/training/ADE_train_00000001.jpg
+        # Its corresponding annotation path is:
+        # .../trainset/annotations/training/ADE_train_00000001.png
+        mask_path = tf.strings.regex_replace(img_path, "Images", "Segmentations")
+        #mask_path = tf.strings.regex_replace(img_path, ".", "_seg_0.")
+        #mask_path = tf.strings.regex_replace(mask_path, "jpg", "png")
+        mask_path = tf.strings.regex_replace(mask_path, ".jpg", "_seg_0.png")
+        mask = tf.io.read_file(mask_path)
+        # The masks contain a class index for each pixels
+        mask = tf.image.decode_png(mask, channels=1)
+        # In scene parsing, "not labeled" = 255
+        # But it will mess up with our N_CLASS = 150
+        # Since 255 means the 255th class
+        # Which doesn't exist
+        mask = tf.where(mask == 255, np.dtype('uint8').type(0), mask)
+        # Note that we have to convert the new value (0)
+        # With the same dtype than the tensor itself
+
+        return {'image': image, 'segmentation_mask': mask}
+
+
+    train_dataset = tf.data.Dataset.list_files((dataset_path / training_data / "*.jpg").as_posix(), seed=SEED)
+    train_dataset = train_dataset.map(parse_image)
+
+    val_dataset = tf.data.Dataset.list_files((dataset_path / val_data / "*.jpg").as_posix(), seed=SEED)
+    val_dataset = val_dataset.map(parse_image)
+
+    @tf.function
+    def normalize(input_image: tf.Tensor, input_mask: tf.Tensor) -> tuple:
+        """Rescale the pixel values of the images between 0.0 and 1.0
+        compared to [0,255] originally.
+
+        Parameters
+        ----------
+        input_image : tf.Tensor
+            Tensorflow tensor containing an image of size [SIZE,SIZE,3].
+        input_mask : tf.Tensor
+            Tensorflow tensor containing an annotation of size [SIZE,SIZE,1].
+
+        Returns
+        -------
+        tuple
+            Normalized image and its annotation.
+        """
+        input_image = tf.cast(input_image, tf.float32) / 255.0
+        return input_image, input_mask
+
+    @tf.function
+    def load_image_train(datapoint: dict) -> tuple:
+        """Apply some transformations to an input dictionary
+        containing a train image and its annotation.
+
+        Notes
+        -----
+        An annotation is a regular  channel image.
+        If a transformation such as rotation is applied to the image,
+        the same transformation has to be applied on the annotation also.
+
+        Parameters
+        ----------
+        datapoint : dict
+            A dict containing an image and its annotation.
+
+        Returns
+        -------
+        tuple
+            A modified image and its annotation.
+        """
+        input_image = tf.image.resize(datapoint['image'], (IMG_SIZE, IMG_SIZE))
+        input_mask = tf.image.resize(datapoint['segmentation_mask'], (IMG_SIZE, IMG_SIZE))
+
+        if tf.random.uniform(()) > 0.5:
+            input_image = tf.image.flip_left_right(input_image)
+            input_mask = tf.image.flip_left_right(input_mask)
+
+        input_image, input_mask = normalize(input_image, input_mask)
+
+        return input_image, input_mask
+
+    @tf.function
+    def load_image_test(datapoint: dict) -> tuple:
+        """Normalize and resize a test image and its annotation.
+
+        Notes
+        -----
+        Since this is for the test set, we don't need to apply
+        any data augmentation technique.
+
+        Parameters
+        ----------
+        datapoint : dict
+            A dict containing an image and its annotation.
+
+        Returns
+        -------
+        tuple
+            A modified image and its annotation.
+        """
+        input_image = tf.image.resize(datapoint['image'], (IMG_SIZE, IMG_SIZE))
+        input_mask = tf.image.resize(datapoint['segmentation_mask'], (IMG_SIZE, IMG_SIZE))
+
+        input_image, input_mask = normalize(input_image, input_mask)
+
+        return input_image, input_mask
+
+
+
+    dataset = {"train": train_dataset, "val": val_dataset}
+
+    # -- Train Dataset --#
+    dataset['train'] = dataset['train'].map(load_image_train)#, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    dataset['train'] = dataset['train'].shuffle(buffer_size=BUFFER_SIZE, seed=SEED)
+    dataset['train'] = dataset['train'].repeat()
+    dataset['train'] = dataset['train'].batch(BATCH_SIZE)
+    dataset['train'] = dataset['train'].prefetch(buffer_size=AUTOTUNE)
+
+    #-- Validation Dataset --#
+    dataset['val'] = dataset['val'].map(load_image_test)
+    dataset['val'] = dataset['val'].repeat()
+    dataset['val'] = dataset['val'].batch(BATCH_SIZE)
+    dataset['val'] = dataset['val'].prefetch(buffer_size=AUTOTUNE)
+
+
+    ##### TRAIN MODEL ##### 
     save_model = False
 
-    unet = Pix2Pix_Unet()
+    batch_size = BATCH_SIZE
+    gf = 32
+    img_size = (IMG_SIZE,IMG_SIZE,3)#(256,256,3)
 
-    unet.train(epochs=5000, batch_size=16, sample_interval_epoch=10, save_interval_epoch=2)
+    num_epochs = 10
+    sample_img_interval = 2
+
+    unet = Pix2Pix_Unet(train_dataset=dataset['train'],img_size=img_size,batch_size=batch_size, gf=gf,test_data=[])
+    
+    ######
+    #EPOCHS = 10
+    #STEPS_PER_EPOCH = 100 // BATCH_SIZE
+    #VALIDATION_STEPS = VALSET_SIZE // BATCH_SIZE
+
+    #model_history = unet.unet.fit(dataset['train'], epochs=EPOCHS,)
+                          #steps_per_epoch=STEPS_PER_EPOCH,)
+                          #validation_steps=VALIDATION_STEPS,
+                          #validation_data=dataset['val'])
+    ######
+    STEPS_PER_EPOCH = 100 // batch_size #there are 100 images in total
+    unet.train(epochs=num_epochs,STEPS_PER_EPOCH=STEPS_PER_EPOCH,sample_interval_epoch=sample_img_interval )
     
     model_name = 'unet_'+datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 
