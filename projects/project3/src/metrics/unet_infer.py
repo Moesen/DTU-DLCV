eee9f64ba9963a6fba191a1d961b38b719cecb1c
Simon Yamazaki <simon-yamaza@hotmail.com>
2022-06-18 17:02:36 +0200

    save a plot of the GT

--- projects/project3/src/metrics/unet_infer.py
+++ projects/project3/src/metrics/unet_infer.py
@@ -86,9 +86,9 @@ for (img, mask, ax) in zip(test_img_plot, mask_img_plot, axs.ravel()):
     out, b_idx = get_boundary(pred_mask.numpy().squeeze(), is_GT=False)
     img[b_idx>1,:] = out[b_idx>1,:]
 
-    green_patch = mpatches.Patch(color='green', label='GT')
-    red_patch = mpatches.Patch(color='red', label='Pred')
-    plt.legend(handles=[green_patch,red_patch])
+    green_patch = mpatches.Patch(color=(0,255,0), label='GT')
+    red_patch = mpatches.Patch(color=(255,0,0), label='Pred')
+    ax.legend(handles=[green_patch,red_patch],bbox_to_anchor=(1.2, 0.5))
 
     ax.imshow(img)
     #ax.get_xaxis().set_ticks([])
@@ -97,13 +97,22 @@ for (img, mask, ax) in zip(test_img_plot, mask_img_plot, axs.ravel()):
     compute_IoU = tf.keras.metrics.IoU(num_classes=2, target_class_ids=[0])
     img_iou = compute_IoU(pred_mask, mask)
 
-    ax.set_title(f"Prediction: {img_iou:.2f}",fontsize=20,x=0.5,y=1.05)
+    ax.set_title(f"IoU: {img_iou:.2f}",fontsize=16,x=0.5,y=1.05)
     ax.grid(False)
     ax.axis('off')
 
 #plt.subplots_adjust(left=0.1, bottom=0.1, right=0.9, top=0.9, wspace=0.2, hspace=0.2)
 
 fig_path = PROJECT_ROOT / "reports/figures/1_boundary.png"
+plt.savefig(fig_path)
+
+
+fig, axs = plt.subplots(1,4, figsize=(15,8))
+
+for n, (mask, ax) in enumerate(zip(mask_img_plot, axs.ravel())):
+    ax.imshow(mask)
+
+fig_path = PROJECT_ROOT / "reports/figures/GT_boundary.png"
 plt.savefig(fig_path)
 
 #compute metrics for model
@@ -121,8 +130,4 @@ for (x_batch_val, true_mask) in val_dataset:
         total_iou.append( batch_iou )
 
 print("IoU for entire test set: ",np.array(total_iou).mean())
-
-
-
-
 
