d2cdbf6a19452d7f9d9cb406e2cf8a1da09f0764
Simon Yamazaki <simon-yamaza@hotmail.com>
2022-06-19 18:46:31 +0200

    plotting more segmentation type unets

--- projects/project3/src/metrics/unet_infer.py
+++ projects/project3/src/metrics/unet_infer.py
@@ -24,7 +24,7 @@ from projects.project3.src.metrics.losses import *
 from projects.project3.src.metrics.eval_metrics import *
 from projects.project3.src.visualization.make_boundary import get_boundary
 
-
+## load the base model
 PROJECT_ROOT = get_project3_root()
 model_name = "unet_20220618123600"
 model_path = PROJECT_ROOT / "models" / model_name
@@ -34,7 +34,26 @@ loss_fn = focal_loss()
 unet = tf.keras.models.load_model(model_path, custom_objects={"loss": loss_fn })
 unet.summary()
 
+### the models trained on other segmentation types 
 
+model_name = "unet2_"
+model_path = PROJECT_ROOT / "models" / model_name
+unet2 = tf.keras.models.load_model(model_path, custom_objects={"loss": loss_fn })
+
+"""model_name = "unet1_"
+model_path = PROJECT_ROOT / "models" / model_name
+unet1 = tf.keras.models.load_model(model_path, custom_objects={"loss": loss_fn })
+
+model_name = "unet0_"
+model_path = PROJECT_ROOT / "models" / model_name
+unet0 = tf.keras.models.load_model(model_path, custom_objects={"loss": loss_fn })"""
+
+unet_models = [unet, unet2]
+unet_seg_type = ["All", "Type 2"]
+###
+
+
+#Initialize dataloader
 BATCH_SIZE = 16
 IMG_SIZE = (256,256)
 
@@ -42,7 +61,6 @@ proot = get_project3_root()
 data_root = proot / "data/isic/test_style0" #train_allstyles" #test_style0"
 image_path = data_root / "Images"
 mask_path = data_root / "Segmentations"
-
 
 dataset_loader = IsicDataSet(
     image_folder=image_path,
@@ -68,50 +86,52 @@ test_img_plot = tf.gather(test_img, idx)
 mask_img_plot = tf.gather(mask, idx)
 
 print("Plotting...")
-fig, axs = plt.subplots(1,4, figsize=(15,8))
+fig, axs = plt.subplots(len(unet_models),4, figsize=(15,6*len(unet_models)))
 
+for m, model in enumerate(unet_models):
+    for n,(img, mask, ax) in enumerate(zip(test_img_plot, mask_img_plot, axs.ravel())):
 
-for n,(img, mask, ax) in enumerate(zip(test_img_plot, mask_img_plot, axs.ravel())):
-    
-    pred_logits = unet.predict(tf.expand_dims(img, 0))
-    pred_probs = tf.keras.activations.sigmoid(pred_logits)
-    pred_mask = tf.math.round(pred_probs)
+        #make segmentation predictions
+        pred_logits = model.predict(tf.expand_dims(img, 0))
+        pred_probs = tf.keras.activations.sigmoid(pred_logits)
+        pred_mask = tf.math.round(pred_probs)
 
-    img_np = img.numpy()
-    mask_np = mask.numpy()
+        img_np = img.numpy()
+        mask_np = mask.numpy()
 
-    #change color for boundary of GT mask 
-    out, b_idx = get_boundary(mask_np, is_GT=True)
-    img_np[b_idx>1,:] = out[b_idx>1,:]
-    
-    #change color for bounadry of prediction
-    out, b_idx = get_boundary(pred_mask.numpy().squeeze(), is_GT=False)
-    img_np[b_idx>1,:] = out[b_idx>1,:]
+        #change color for boundary of GT mask 
+        out, b_idx = get_boundary(mask_np, is_GT=True)
+        img_np[b_idx>1,:] = out[b_idx>1,:]
+        
+        #change color for bounadry of prediction
+        out, b_idx = get_boundary(pred_mask.numpy().squeeze(), is_GT=False)
+        img_np[b_idx>1,:] = out[b_idx>1,:]
 
-    gc = mpl.colors.to_rgba((0,1,0))
-    rc = mpl.colors.to_rgba((1,0,0))
+        ax.imshow(img_np)
 
-    green_patch = mpatches.Patch(color=gc, label='GT')
-    red_patch = mpatches.Patch(color=rc, label='Pred')
+        if m==0:
+            ax.set_ylabel(unet_seg_type[m], rotation='horizontal', fontsize=16, ha='right')
 
-    if n==2:
-        ax.legend(handles=[green_patch,red_patch], bbox_to_anchor=(0.7, -0.05), ncol=2)
+        #make manual legend
+        if n==2 and m==len(unet_models)-1:
+            gc = mpl.colors.to_rgba((0,1,0))
+            rc = mpl.colors.to_rgba((1,0,0))
+            green_patch = mpatches.Patch(color=gc, label='GT')
+            red_patch = mpatches.Patch(color=rc, label='Pred')
+            ax.legend(handles=[green_patch,red_patch], bbox_to_anchor=(0.7, -0.05), ncol=2)
 
-    ax.imshow(img_np)
-    #ax.get_xaxis().set_ticks([])
-    #ax.get_yaxis().set_ticks([])
+        #plot the iou and area difference in title
+        compute_IoU = tf.keras.metrics.IoU(num_classes=2, target_class_ids=[0])
+        img_iou = compute_IoU(pred_mask, mask)
 
-    compute_IoU = tf.keras.metrics.IoU(num_classes=2, target_class_ids=[0])
-    img_iou = compute_IoU(pred_mask, mask)
+        n_seg_pixels_mask = tf.math.reduce_sum(mask).numpy()
+        n_seg_pixels_pred = tf.math.reduce_sum(pred_mask).numpy()
 
-    n_seg_pixels_mask = tf.math.reduce_sum(mask).numpy()
-    n_seg_pixels_pred = tf.math.reduce_sum(pred_mask).numpy()
+        p_diff = (n_seg_pixels_pred - n_seg_pixels_mask) / n_seg_pixels_mask
 
-    p_diff = (n_seg_pixels_pred - n_seg_pixels_mask) / n_seg_pixels_mask
-
-    ax.set_title(f"IoU: {img_iou:.2f}, Area diff: {p_diff:.2f}",fontsize=16,x=0.5,y=1.05)
-    ax.grid(False)
-    ax.axis('off')
+        ax.set_title(f"IoU: {img_iou:.2f}, Area diff: {p_diff:.2f}",fontsize=16,x=0.5,y=1.05)
+        ax.grid(False)
+        ax.axis('off')
 
 plt.subplots_adjust(left=0.1, bottom=0.1, right=0.9, top=0.9, wspace=0.2, hspace=0.2)
 
@@ -119,6 +139,7 @@ fig_path = PROJECT_ROOT / "reports/figures/1_boundary.png"
 plt.savefig(fig_path,bbox_inches='tight')
 
 
+#plot a figure of the GT segmentations to check implementation
 fig, axs = plt.subplots(1,4, figsize=(15,8))
 
 for n, (mask, ax) in enumerate(zip(mask_img_plot, axs.ravel())):
@@ -126,6 +147,7 @@ for n, (mask, ax) in enumerate(zip(mask_img_plot, axs.ravel())):
 
 fig_path = PROJECT_ROOT / "reports/figures/GT_boundary.png"
 plt.savefig(fig_path)
+
 
 #compute metrics for model
 total_iou = []
