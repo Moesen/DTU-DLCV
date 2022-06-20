658c2375c7f08a7d563c108439bd924d86ebe4e6
Moesen <gustavmoesmand@gmail.com>
2022-06-20 17:50:56 +0200

    added timing almost done

--- projects/project3/src/models/tune_unet.py
+++ projects/project3/src/models/tune_unet.py
@@ -6,6 +6,7 @@ import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 import json
+import time
 from pathlib import Path
 
 import matplotlib.pyplot as plt
@@ -21,8 +22,6 @@ from projects.project3.src.metrics.losses import *
 from projects.project3.src.models.Networks import Pix2Pix_Unet
 from projects.utils import get_project3_root
 from tensorflow import keras
-from tensorflow.python.client import device_lib
-from tqdm import tqdm
 from wandb.keras import WandbCallback
 
 log_path = Path("./log")
@@ -30,11 +29,41 @@ logger = init_logger(__name__, True, log_path)
 
 study_name = "unet_test_100"
 
+NUM_EPOCHS = 100
 IMG_SIZE = (256, 256)  # (256,256,3)
+
+class Timer():
+    def __init__(self):
+        self._last_time = time.time()
+        self._timings = []
+        self._sections = []
+        self._training_time = []
+        self._epoch_sections = []
+    
+    def add_new(self, name: str):
+        self._timings += [time.time() - self._last_time]
+        self._sections += [name]
+        self._last_time = time.time()
+
+    def add_new_training_time(self, epoch: str | int):
+        self._epoch_sections += [epoch]
+        self._training_time += [time.time() - self._last_time]
+        self._last_time = time.time()
+        
+    def log_time(self):
+        log_msg = "#" * 10 + " " * 5 + "TIMINGS" + " " * 5 + "#" * 10 + "\n" 
+        for s, t in zip(self._sections, self._timings):
+            log_msg += f"\t{s}: \t{t} seconds \t\n"
+        log_msg += "#" * 10 + " " * 5 + "TRAINING" + " " * 5 + "#" * 10 + "\n" 
+        for s, t in zip(self._epoch_sections, self._training_time):
+            log_msg += f"\t{s}: \t{t} seconds \t\n"
+        logger.info(log_msg) 
+
 
 
 def objective(trial: optuna.trial.Trial) -> float:
     logger.info(f"starting {trial.number = }")
+    timer = Timer() 
 
     # config
     c = dict(
@@ -51,9 +80,6 @@ def objective(trial: optuna.trial.Trial) -> float:
         loss_func=trial.suggest_categorical(
             "Loss function", ["focal_loss", "dice_loss", "weighted_cross_entropy"]
         ),
-        # "cross_entroy": tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-        # Img attributes
-        # image_size=trial.suggest_int("image size", 64, 256, 16),
         # Augmentations
         # augmentation_flip=trial.suggest_categorical(
         #    "augmentation_flip",
@@ -62,6 +88,7 @@ def objective(trial: optuna.trial.Trial) -> float:
         # augmentation_rotation=trial.suggest_float("augmentation_rotation", 0.0, 0.2),
         # augmentation_contrast=trial.suggest_float("augmentation_contrast", 0.0, 0.6),
     )
+    timer.add_new("creating config")
     switch = {
         "focal_loss": focal_loss,
         "dice_loss": dice_loss,
@@ -76,7 +103,7 @@ def objective(trial: optuna.trial.Trial) -> float:
     data_root = Path("/dtu/datasets1/02514/isic/train_allstyles")
     image_path = data_root / "Images"
     mask_path = data_root / "Segmentations"
-
+    
     dataset_loader = IsicDataSet(
         image_folder=image_path,
         mask_folder=mask_path,
@@ -89,10 +116,13 @@ def objective(trial: optuna.trial.Trial) -> float:
         validation_percentage=0.2,
         seed=69,
     )
+    timer.add_new("Dataset class")
 
     train_dataset, val_dataset = dataset_loader.get_dataset(
         batch_size=c["batch_size"], shuffle=True
     )
+
+    timer.add_new("Dataset Loader")
 
     run = wandb.init(
         project="project3",
@@ -101,6 +131,8 @@ def objective(trial: optuna.trial.Trial) -> float:
         config=c,
         reinit=True,
     )
+
+    timer.add_new("Wandb")
 
     # metrics
     # metric = tf.keras.metrics.IoU(num_classes=2, target_class_ids=[0])
@@ -118,7 +150,7 @@ def objective(trial: optuna.trial.Trial) -> float:
         batchnorm=c["batchnorm"],
     )
 
-    unet.unet.summary()
+    timer.add_new("Unet creation")
 
     def log_image(epoch, logs):
         (x_batch_val, y_batch_val) = next(iter(val_dataset))
@@ -143,12 +175,10 @@ def objective(trial: optuna.trial.Trial) -> float:
             plt.axis("off")
         wandb.log({"Validation:": plt}, step=epoch)
 
-    image_callback = keras.callbacks.LambdaCallback(on_epoch_end=log_image)
-
-    num_epochs = 100
-    # unet.train(epochs=num_epochs)
-
     # Callbacks
+    image_callback = keras.callbacks.LambdaCallback(on_epoch_end=log_image)
+    wandb_callback = WandbCallback(monitor="val_loss", log_evaluation=False, save_model=False, validation_steps = len(val_dataset))
+    image_callback = keras.callbacks.LambdaCallback(on_epoch_end=log_image)
     early_stopping = tf.keras.callbacks.EarlyStopping(
         monitor="val_loss",
         patience=10,
@@ -156,8 +186,8 @@ def objective(trial: optuna.trial.Trial) -> float:
         mode="min",
         restore_best_weights=True
     )
-    wandb_callback = WandbCallback(monitor="val_loss", log_evaluation=False, save_model=False, validation_steps = len(val_dataset))
-    image_callback = keras.callbacks.LambdaCallback(on_epoch_end=log_image)
+
+    timer.add_new("Callback init")
     history = unet.unet.fit(
         train_dataset,
         validation_data=val_dataset,
